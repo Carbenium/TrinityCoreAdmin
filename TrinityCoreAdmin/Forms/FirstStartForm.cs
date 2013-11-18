6541dd4d0cb12caf747e311eb00d70606e168621
Carbenium <keresztesschmidt@gmail.com>
2013-11-18 23:10:21 +0100

    Add FirstStartForm

---
+++ TrinityCoreAdmin/Forms/FirstStartForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrinityCoreAdmin.Forms
+{
+    public partial class FirstStartForm : Form
+    {
+        public FirstStartForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
