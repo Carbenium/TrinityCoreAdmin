5cdeb71bf8779e6f7bb7f1dab854d73ba415adc3
Carbenium <keresztesschmidt@gmail.com>
2013-12-01 17:53:56 +0100

    Set CancelButton for RelamManagerForm

--- TrinityCoreAdmin/Forms/RealmManagerForm.Designer.cs
+++ TrinityCoreAdmin/Forms/RealmManagerForm.Designer.cs
@@ -125,6 +125,7 @@
             // 
             // btnCancel
             // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Location = new System.Drawing.Point(404, 364);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
@@ -482,6 +483,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(498, 399);
             this.Controls.Add(this.tabRealmManager);
             this.Controls.Add(this.btnNewServer);
